a9b864ccd5dcdf92583d6759913c25240e175ec2
Imam-f <mohamadimamfirdaus17@gmail.com>
2024-11-26 00:51:28 +0700

    solve serialization problem in windows

--- pipe_operator_parallel.py
+++ pipe_operator_parallel.py
@@ -5,6 +5,8 @@ import os
 import dill
 import concurrent.futures as ft
 
+import inspect
+
 # This one doesnt work:
 # Reason 1. local nested function cannot be pickled [done]
 # Reason 2. decorator cannot be pickled [done in linux][todo: windows]
@@ -15,12 +17,18 @@ def feeder(input_data, queue):
         queue.put(item)
     queue.put(None)  # Sentinel value to indicate the end
 
-def stage_worker(input_queue, output_queue, func):
+def func(*args, **kwargs):
+    pass
+
+def stage_worker(input_queue, output_queue, func_str):
     
     # if os.name == "nt":
     #     func = dill.dumps(func)
     # else:
     #     func = func
+    # print(func_str)
+    exec(func_str, globals())
+    # print(func)
     while True:
         item = input_queue.get()
         if item is None:
@@ -65,18 +73,22 @@ class Pipeline:
         processes.append(feeder_process)
 
         # Start each stage in a separate process
-
         if os.name == "nt":
-            print("The system is Windows.")
-            pool = ft.ProcessPoolExecutor(
-                max_workers=8
-            )
+            # print("The system is Windows.")
+            # pool = ft.ProcessPoolExecutor(
+            #     max_workers=8
+            # )
             for i, func in enumerate(self.stages):
                 # func = dill.dumps(func)
-                result = pool.submit(stage_worker, (queues[i], queues[i+1], func)).result()
-                # p = multiprocessing.Process(target=stage_worker, args=(queues[i], queues[i+1], func))
-                # p.start()
-                processes.append(result)
+                source = inspect.getsource(func)
+                source = source.split("\n")[1:]
+                source[0] = source[0].replace(source[0].split(" ")[1].split("(")[0], "func")
+                source = "\n".join(source)
+                # print(source)
+                # result = pool.submit(stage_worker, (queues[i], queues[i+1], source)).result()
+                p = multiprocessing.Process(target=stage_worker, args=(queues[i], queues[i+1], source))
+                p.start()
+                processes.append(p)
         else:
             for i, func in enumerate(self.stages):
                 p = multiprocessing.Process(target=stage_worker, args=(queues[i], queues[i+1], func))
--- pipe_operator_parallel_lazy.py
+++ pipe_operator_parallel_lazy.py
@@ -1,4 +1,9 @@
 import multiprocessing
+import os
+import inspect
+
+def func(*args, **kwargs):
+    pass
 
 def pipeable(func):
     # Decorator to make functions pipeable
@@ -30,7 +35,8 @@ def feeder(input_data, queue):
         queue.put(item)
     queue.put(None)  # Sentinel value to indicate the end
 
-def stage_worker(input_queue, output_queue, func):
+def stage_worker(input_queue, output_queue, func_str):
+    exec(func_str, globals())
     while True:
         item = input_queue.get()
         if item is None:
@@ -95,10 +101,27 @@ class Pipeline:
         self.processes.append(feeder_process)
 
         # Stage processes
-        for i, func in enumerate(self.stages):
-            p = multiprocessing.Process(target=stage_worker, args=(self.queues[i], self.queues[i+1], func))
-            p.start()
-            self.processes.append(p)
+        if os.name == "nt":
+            # print("The system is Windows.")
+            # pool = ft.ProcessPoolExecutor(
+            #     max_workers=8
+            # )
+            for i, func in enumerate(self.stages):
+                # func = dill.dumps(func)
+                source = inspect.getsource(func)
+                source = source.split("\n")[1:]
+                source[0] = source[0].replace(source[0].split(" ")[1].split("(")[0], "func")
+                source = "\n".join(source)
+                # print(source)
+                # result = pool.submit(stage_worker, (queues[i], queues[i+1], source)).result()
+                p = multiprocessing.Process(target=stage_worker, args=(self.queues[i], self.queues[i+1], source))
+                p.start()
+                self.processes.append(p)
+        else:
+            for i, func in enumerate(self.stages):
+                p = multiprocessing.Process(target=stage_worker, args=(self.queues[i], self.queues[i+1], func))
+                p.start()
+                self.processes.append(p)
 
         # Output collector
         return output_collector(self.queues[-1])
