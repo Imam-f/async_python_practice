ec1392cb0b5d2e359a34b5ba28bc467f0826eb74
Imam-f <mohamadimamfirdaus17@gmail.com>
2025-07-12 22:57:59 +0700

    fix remote ssh proxy

--- lib/recursive_rpc/test/test.py
+++ lib/recursive_rpc/test/test.py
@@ -31,25 +31,21 @@ def main():
                         PORT, 
                         PASSWORD, 
                         remote_port[0])
+    stop()
 
-    stop2 = activate_ssh(HOSTNAME, 
-                        USER, 
-                        PORT, 
-                        PASSWORD, 
-                        remote_port[1])
-    
     HOSTNAME_FORWARD = HOSTNAME
     PORT_FORWARD = PORT
     ssh_login = (USER, PASSWORD)
+    print(HOSTNAME_FORWARD, USER, PORT_FORWARD, ssh_login)
     
     with Recursive_RPC(client=[
-                proxyprocess(remote_port[2], HOSTNAME_FORWARD, PORT_FORWARD, [
-                    localprocess(4),
-                    networkprocess(2, HOSTNAME, remote_port[1], stop2)
-                ], ssh_login),
-                localprocess(2),
-                networkprocess(4, HOSTNAME, remote_port[0], stop)
-            ]) as pool:
+                proxyprocess(remote_port[1], HOSTNAME_FORWARD, PORT_FORWARD, [
+                    # localprocess(4),
+                    networkprocess(2, HOSTNAME, remote_port[2], "tag1")
+                ], ssh_login, {"tag1": (HOSTNAME, USER, PORT, PASSWORD, remote_port[2])}),
+                # localprocess(2),
+                # networkprocess(4, HOSTNAME, remote_port[0], stop)
+            ], conn={}) as pool:
         
         # Create a list of numbers to process
         numbers = list(range(10))
